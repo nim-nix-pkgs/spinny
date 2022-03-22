a41aef0aaf1abd0c6088050371db29052b2e7b0a
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:57 -0300

    chore: re-index ../nimpkgs/s/spinny

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Spinny is a tiny terminal spinner package for the Nim Programming Language.'';
+  description = ''Terminal Spinners for Nim.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-spinny-master.flake = false;
   inputs.src-spinny-master.owner = "Yardanico";
-  inputs.src-spinny-master.ref   = "refs/heads/master";
+  inputs.src-spinny-master.ref   = "master";
   inputs.src-spinny-master.repo  = "spinny";
   inputs.src-spinny-master.type  = "github";
   
